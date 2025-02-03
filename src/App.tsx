7c3d327de99df1af33fed22822c8507b165ba960
ReaksmeyThan <reaksmey.than7up@gmail.com>
2025-02-03 18:10:32 +0700

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-
-import { Button } from "@chakra-ui/react"
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-
   return (
-    <Button asChild>
-    <a href="#">Button</a>
-  </Button>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral" border="1px solid black">
+        Nav area
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="yellow" border="1px solid black">
+          Aside area
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="red" border="1px solid black">
+        Main area
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Provider } from "@/components/ui/provider";
+import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -6,8 +6,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>
 );
